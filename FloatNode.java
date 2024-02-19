b56df0966352dfc7272addd0d001fca0eb5344c8
Noah <noahclough@outlook.com>
2024-02-19 14:43:05 -0500

    add files

---
+++ FloatNode.java
@@ -0,0 +1,10 @@
+package parser;
+
+public class FloatNode extends Node{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}	
---
+++ Node.java
@@ -0,0 +1,4 @@
+package parser;
+public abstract class Node {
+	public abstract String toString();
+}
